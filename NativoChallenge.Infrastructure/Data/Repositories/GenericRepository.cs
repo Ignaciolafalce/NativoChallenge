fd8813feb599dafa947c6dc60be161c9152cf98b
Ignacio lafalce <nacho_lafalce@hotmail.com>
2025-06-02 14:35:55 -0300

    Generic and Task Repositories implementations added

---
+++ NativoChallenge.Infrastructure/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using NativoChallenge.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using NativoChallenge.Infrastructure.Data.EF;
+
+namespace NativoChallenge.Infrastructure.Data.Repositories;
+
+public class GenericRepository<T, TId> : IGenericRepository<T, TId> where T : class
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public async Task<T> AddAsync(T entity, CancellationToken cancellationToken)
+    {
+        _dbSet.Add(entity);
+        await _context.SaveChangesAsync(cancellationToken);
+        return entity;
+    }
+
+    public async Task<T?> GetByIdAsync(TId id, CancellationToken cancellationToken)
+    {
+        return await _dbSet.FindAsync([id], cancellationToken);
+    }
+
+    public async Task DeleteAsync(TId id, CancellationToken cancellationToken)
+    {
+        var entity = await GetByIdAsync(id, cancellationToken) ?? throw new InvalidOperationException("Entity not found");
+
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await _dbSet.ToListAsync(cancellationToken); // can be optimized with iqueryable, sorry :(
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
+    {
+        return await _dbSet.Where(predicate).ToListAsync(cancellationToken); // can be optimized with iqueryable, very sorry :(
+    }
+
+    public async Task UpdateAsync(T entity, CancellationToken cancellationToken)
+    {
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ NativoChallenge.Infrastructure/Data/Repositories/TaskRepository.cs
@@ -0,0 +1,23 @@
+﻿using NativoChallenge.Domain.Enums;
+using NativoChallenge.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using NativoChallenge.Infrastructure.Data.EF;
+using Entities = NativoChallenge.Domain.Entities;
+
+namespace NativoChallenge.Infrastructure.Data.Repositories;
+
+public class TaskRepository : GenericRepository<Entities.Task, Guid>, ITaskRepository
+{
+    public TaskRepository(AppDbContext context) : base(context) { }
+
+    public async Task<int> CountHighPriorityPendingAsync(CancellationToken cancellationToken)
+    {
+        var counter = await _dbSet.CountAsync(t => t.Priority == TaskPriority.High && t.State == TaskState.Pending, cancellationToken);
+        return counter;
+    }
+
+    public async Task<Entities.Task> CreateAsync(Entities.Task task, CancellationToken cancellationToken)
+    {
+        return await AddAsync(task, cancellationToken); // i'm just wrapping the generic method ;), fede?
+    }
+}
