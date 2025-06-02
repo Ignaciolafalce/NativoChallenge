20c84ba8779e37f37f7c568a9bd047a740421fa1
Ignacio lafalce <nacho_lafalce@hotmail.com>
2025-06-02 12:24:23 -0300

    Generic and Task Repositories interfaces added

---
+++ NativoChallenge.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace NativoChallenge.Domain.Interfaces;
+
+public interface IGenericRepository<T, TId> where T : class
+{
+    Task<T> AddAsync(T entity, CancellationToken cancellationToken);
+    Task<T?> GetByIdAsync(TId id, CancellationToken cancellationToken);
+    Task DeleteAsync(TId id, CancellationToken cancellationToken);
+    Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken);
+    Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken);
+    Task UpdateAsync(T entity, CancellationToken cancellationToken);
+}
---
+++ NativoChallenge.Domain/Interfaces/ITaskRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace NativoChallenge.Domain.Interfaces;
+
+public interface ITaskRepository : IGenericRepository<Entities.Task, Guid>
+{
+    Task<int> CountHighPriorityPendingAsync(CancellationToken cancellationToken);
+    Task<Entities.Task> CreateAsync(Entities.Task task, CancellationToken cancellationToken);
+}
