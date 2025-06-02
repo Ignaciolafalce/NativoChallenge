5bd1aeef4511e0164e05543d1f7b15d8cb86572f
Ignacio lafalce <nacho_lafalce@hotmail.com>
2025-06-02 14:05:56 -0300

    Configuration Services for DI (ServiceCollectionExtension) added

---
+++ NativoChallenge.Application/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NativoChallenge.Application.Common;
+using System.Reflection;
+
+namespace NativoChallenge.Application.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        // CQRS con MediatR
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        // FluentValidation
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        // Validation pipeline with FluentValidation + MediatR
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
