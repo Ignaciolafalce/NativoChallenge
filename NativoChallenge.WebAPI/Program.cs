1b09ee4a3975eb4e8165801b7794557d44643bdd
Ignacio lafalce <nacho_lafalce@hotmail.com>
2025-06-02 14:56:13 -0300

    App, Infra, OpenApi, Swagger, Task Endpoints (services/middlewares) added to Program.cs :)

--- NativoChallenge.WebAPI/Program.cs
+++ NativoChallenge.WebAPI/Program.cs
@@ -1,36 +1,88 @@
 
-namespace NativoChallenge.WebAPI
+using Microsoft.OpenApi.Models;
+using NativoChallenge.Infrastructure.Data.Configuration;
+using NativoChallenge.Application.Configuration;
+using NativoChallenge.Infrastructure.Data.EF;
+using NativoChallenge.WebAPI.Endpoints;
+
+namespace NativoChallenge.WebAPI;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        #region Services 
+
+        // Application and Infrastructure services
+        builder.Services.AddApplication(builder.Configuration);
+        builder.Services.AddInfrastructure(builder.Configuration);
+
+        // OpenApi and Swagger services
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            options.SwaggerDoc("v1", new OpenApiInfo
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                Title = "Nativo Challenge API",
+                Version = "v1",
+                Description = "API para gestión de tareas usando Clean Architecture.",
+                Contact = new OpenApiContact
+                {
+                    Name = "Ignacio Lafalce",
+                    Email = "nacho_lafalce@hotmail.com"
+                }
+            });
+        });
 
-            app.UseHttpsRedirection();
+        #endregion 
 
-            app.UseAuthorization();
+        var app = builder.Build();
 
+        // Seed Task data
+        SeedTasksData(app);
 
-            app.MapControllers();
+        #region Http Request Pipeline Configuration - Middlewares
 
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            //app.UseSwagger();
+            //app.UseSwaggerUI();
+        }
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Nativo Challenge API v1");
+            options.DocumentTitle = "Nativo Challenge";
+        });
+
+        app.UseHttpsRedirection();
+
+        // Redirect root path to Swagger UI
+        app.MapGet("/", () => Results.Redirect("/swagger"));
+
+        app.UseAuthorization(); // not really necessary, but we can improve auth and auth later...
+
+        app.MapTaskEndpoints();
+
+        app.Run();
+        #endregion
+    }
+
+    #region Private utilities
+
+    // this could be in AddInfrastructe(...)? i prefer here . . .
+    private static async void SeedTasksData(WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            await SeedData.SeedAsync(appDbContext);
         }
     }
+    #endregion
+
 }
