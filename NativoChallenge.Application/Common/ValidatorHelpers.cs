e811b3c01f5bcab3ac0c95d2f5e5e8dabeb67664
Ignacio lafalce <nacho_lafalce@hotmail.com>
2025-06-02 14:06:30 -0300

    ValidationBehavior class neccesary for DI Pipeline

---
+++ NativoChallenge.Application/Common/ValidatorHelpers.cs
@@ -0,0 +1,10 @@
+﻿namespace NativoChallenge.Application.Common;
+
+public static class ValidatorHelpers
+{
+    public static bool IsGuidType(string field)
+    {
+        var parseResult = Guid.TryParse(field, out _);
+        return parseResult;
+    }
+}
