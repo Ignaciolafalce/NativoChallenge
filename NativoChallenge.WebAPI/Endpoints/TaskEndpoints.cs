a1ac147570a4257ae2fe6a8c90e712f69545037a
Ignacio lafalce <nacho_lafalce@hotmail.com>
2025-06-02 14:55:03 -0300

    Task endpoints (IEndpointRouteBuilder extension method) added

---
+++ NativoChallenge.WebAPI/Endpoints/TaskEndpoints.cs
@@ -0,0 +1,44 @@
+﻿using NativoChallenge.Application.Tasks.Commands;
+using NativoChallenge.Application.Tasks.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NativoChallenge.WebAPI.Endpoints;
+
+public static class TaskEndpoints
+{
+    public static IEndpointRouteBuilder MapTaskEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/tasks");
+
+        // GET /tasks?state=Pending&orderBy=priority
+        group.MapGet("/", async (ISender sender, [AsParameters] ListTasksQuery query) =>
+        {
+            var result = await sender.Send(query);
+            return Results.Ok(result);
+        });
+
+        // POST /tasks
+        group.MapPost("/", async (ISender sender, [FromBody] CreateTaskCommand command) =>
+        {
+            var result = await sender.Send(command);
+            return Results.Ok(result);
+        });
+
+        // DELETE /tasks/{id}
+        group.MapDelete("/{id:guid}", async (ISender sender, [FromRoute] Guid id) =>
+        {
+            await sender.Send(new DeleteTaskCommand(id));
+            return Results.NoContent();
+        });
+
+        // PUT /tasks/{id}/complete
+        group.MapPut("/{id:guid}/complete", async (ISender sender, [FromRoute] Guid id) =>
+        {
+            await sender.Send(new CompleteTaskCommand(id));
+            return Results.NoContent();
+        });
+
+        return app;
+    }
+}
