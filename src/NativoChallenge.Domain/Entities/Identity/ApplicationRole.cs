18e634121a5974e4b8cb17f3beebd42c6807981d
Ignacio lafalce <nacho_lafalce@hotmail.com>
2025-06-11 01:52:59 -0300

    Domain: User and Role added (EF Identity)

---
+++ src/NativoChallenge.Domain/Entities/Identity/ApplicationRole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NativoChallenge.Domain.Entities.Identity
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+        public ApplicationRole() : base() { }
+        public ApplicationRole(string roleName) : base(roleName) { }
+
+    }
+
+}
